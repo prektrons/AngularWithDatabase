f1122eaa53dd6e949e9b88ea5976fe87587b656c
Preksha Sharma <46593513+prektrons@users.noreply.github.com>
2022-04-24 12:42:32 +0530

    Setting up database and bootstrap

---
+++ firebaseCRUDApp/src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase:{
+      apiKey: "AIzaSyC3WdeFqVi7EL7h5hoYq9RcCvopLfU-ZxU",
+      authDomain: "book-29227.firebaseapp.com",
+      projectId: "book-29227",
+      storageBucket: "book-29227.appspot.com",
+      messagingSenderId: "724396501140",
+      appId: "1:724396501140:web:59cdd0c01f7db738bdec81"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
