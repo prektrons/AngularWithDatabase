ffca308dd78e97b31f5e6720c2e92710e8b04c50
Preksha Sharma <46593513+prektrons@users.noreply.github.com>
2022-07-02 16:09:57 +0530

    Created Crud Components

--- my-crud-app/src/app/post/post.module.ts
+++ my-crud-app/src/app/post/post.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PostRoutingModule } from './post-routing.module';
+import { IndexComponent } from './index/index.component';
+import { ViewComponent } from './view/view.component';
+import { CreateComponent } from './create/create.component';
+import { EditComponent } from './edit/edit.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    IndexComponent,
+    ViewComponent,
+    CreateComponent,
+    EditComponent
+  ],
   imports: [
     CommonModule,
     PostRoutingModule
